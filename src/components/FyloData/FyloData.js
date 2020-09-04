706f6720072b3f7cf41705e7d86538ab6b09e03d
Udaya Prakash N <udayaprakash2899@gmail.com>
2020-09-04 20:24:13 +0530

    :heavy_plus_sign: Added tailwind setup

---
+++ src/components/FyloData/FyloData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FyloData = () => {
+    return (
+        <div>
+            Fylodata
+        </div>
+    )
+}
+
+export default FyloData
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import FyloData from '../components/FyloData/FyloData';
+const Home = () => {
+    return (
+        <div>
+            <FyloData/>
+        </div>
+    )
+}
+
+export default Home
